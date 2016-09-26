5eb59a679aa7d7b6ad8af33abdb14582d64aabfb
Reid Kostenuk <reidkostenuk@Reids-MacBook-Pro.local>
2016-09-26 16:39:15 -0400

    First commit

--- Weather App Using API/ViewController.swift
+++ Weather App Using API/ViewController.swift
@@ -9,6 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func submitButtonPressed(_ sender: AnyObject) {
+    
+        let city = cityTextField.text!.replacingOccurrences(of: " ", with: "%20")
+        
+        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(city),uk&appid=c3c7ae57f68a0af00690f6628554d1d5") {
+        
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+                if error != nil {
+                
+                    print(error)
+                
+                } else {
+                
+                    if let urlContent = data {
+                    
+                        do {
+                        
+                            let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        
+                            print(jsonResult)
+                        
+                        
+                            if let description = (((jsonResult as? NSDictionary)?["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String{
+                            
+                                print(description)
+                                    
+                                DispatchQueue.main.sync(execute: {
+                                    
+                                    self.resultLabel.text = description
+                                
+                                })
+                            
+                            }
+                        
+                        } catch {
+                        
+                            print("JSON Processing Failed")
+                        
+                        }
+                    
+                    }
+                
+                }
+                
+            }
+        
+            task.resume()
+        
+        } else {
+            
+            resultLabel.text = "Couldn't find the weather for that city, please try another."
+            
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
